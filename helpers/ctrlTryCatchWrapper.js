6efd8a2c25365962072ceefcef3e98bc6b4324c0
RuslanBoiko <boyko.ruslan78@gmail.com>
2023-07-03 01:07:37 +0300

    hw #2

---
+++ helpers/ctrlTryCatchWrapper.js
@@ -0,0 +1,13 @@
+const ctrlTryCatchWrapper = (ctrl) => {
+  const func = async (req, res, next) => {
+    try {
+      await ctrl(req, res, next);
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  return func;
+};
+
+module.exports = ctrlTryCatchWrapper;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,18 @@
-const express = require('express')
+const express = require("express");
+const router = express.Router();
 
-const router = express.Router()
+const {
+  getAllContacts,
+  getContact,
+  postContact,
+  deleteContact,
+  putContact,
+} = require("../../controllers/contacts");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", getAllContacts);
+router.get("/:contactId", getContact);
+router.post("/", postContact);
+router.delete("/:contactId", deleteContact);
+router.put("/:contactId", putContact);
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-module.exports = router
+module.exports = router;
